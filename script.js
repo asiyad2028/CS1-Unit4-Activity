ec9cf7c3f91858c417bd946634bd41f8abd3fa8d
asiyad2028 <asiya.dowdy2028@gbwl.org>
2026-05-06 18:59:44 +0000

    toggleBtn

--- script.js
+++ script.js
@@ -14,3 +14,35 @@ const toggleBtn = document.querySelector("#toggle");
 heading.style.color = "purple"; //JS wrote CSS code
 heading.textContent = "Light Mode & Dark Mode Website"; 
 
+//Define what happens when button is clicked 
+function toggleMode(){
+console.log("function triggered");
+// 1. flip the boolean value 
+darkMode = !darkMode; 
+console.log(darkMode);
+// 2. Conditionally apply styles based on mode 
+if (darkMode==true){
+    console.log("apply dark mode styles");
+    body.style.background = "black";
+    heading.style.color = "lightgrey";
+    description.style.color = "white";
+    description.textContent = "Dark Mode";
+    toggleBtn.textContent = "switch to light";
+    toggleBtn.style.color= "darkblue";
+    toggleBtn.style.background= "lightblue"
+}
+else{
+    console.log("apply light mode styles");
+    body.style.background = "white";
+    heading.style.color= "black";
+    description.style.color= "purple";
+    description.textContent = "Light Mode";
+    toggleBtn.textContent= "switch to dark";
+    toggleBtn.style.color= "darkpurple";
+    toggleBtn.style.background ="lightpurple";
+
+}
+
+}
+// Attach the function to the button 
+toggleBtn.addEventListener("click",toggleMode); 
